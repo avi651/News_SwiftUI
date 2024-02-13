0810af0a7f89d33bf39fe63822f66204d9b5e051
Avinash Kumar <kumaravi651@gmail.com>
2024-02-13 18:10:12 +0530

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Avinash on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
